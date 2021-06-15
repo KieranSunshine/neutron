388a86961c825432c8983ea19cc0cda6cdcd3c40
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-06-15 20:37:32 +0100

    Added element-plus

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-	},
-	extends: [
-		'plugin:vue/vue3-essential',
-		'@vue/airbnb',
-		'@vue/typescript/recommended',
-	],
-	parserOptions: {
-		ecmaVersion: 2020,
-	},
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-		'no-tabs': ['error', { 'allowIndentationTabs': true }],
-		'vue/script-indent': ['error', 'tab', { 'baseIndent': 1 }],
-		'vue/html-indent': ['error', 'tab', { 'baseIndent': 1 }],
-	},
-	overrides: [
-		{
-			'files': ['*.vue'],
-			'rules': {
-				'indent': 'off'
-			}
-		}
-	]
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-tabs': ['error', { allowIndentationTabs: true }],
+    'vue/script-indent': ['error', 'tab', { baseIndent: 1 }],
+    'vue/html-indent': ['error', 'tab', { baseIndent: 1 }],
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+      },
+    },
+  ],
 };
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<img src="./assets/logo.png">
+	<div>
+		<p>
+			If Element Plus is successfully added to this project, you'll see an
+			<code v-text="'<el-button>'"></code>
+			below
+		</p>
+		<el-button type="primary">el-button</el-button>
+	</div>
+	<HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+	import HelloWorld from './components/HelloWorld.vue';
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+	export default {
+		name: 'App',
+		components: {
+			HelloWorld,
+		},
+	};
 </script>
 
 <style>
