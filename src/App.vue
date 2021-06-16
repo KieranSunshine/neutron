b2f77d516c6008fb449c153d768c8cbfe180023e
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-06-17 00:21:08 +0100

    initial templating

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-	<div></div>
+	<el-container id="app">
+		<neutron-main />
+	</el-container>
 </template>
 
 <script>
+	import NeutronMain from './views/NeutronMain.vue';
+
 	export default {
 		name: 'App',
-		components: {},
+		components: {
+			NeutronMain,
+		},
 	};
 </script>
 
-<style>
+<style lang="scss">
+	@import './assets/colours';
+	@import './assets/utility';
+
+	body {
+		@include max-height;
+
+		margin: 0;
+	}
+
+	#app {
+		@include max-height;
+
+		background: $background;
+	}
 </style>
---
+++ src/views/NeutronMain.vue
@@ -0,0 +1,46 @@
+<template>
+	<el-main id="main">
+		<el-row id="main_row">
+			<!-- Need to implement a multi-pane to make these resizeable -->
+			<!-- Consisting of a multipane component with multipane-separators dividing the content-->
+			<el-col :span="8" class="bordered-column">
+				<request-history />
+			</el-col>
+			<el-col :span="8" class="bordered-column">
+
+			</el-col>
+			<el-col :span="8">
+
+			</el-col>
+		</el-row>
+	</el-main>
+</template>
+
+<script>
+	import RequestHistory from '../components/RequestHistory.vue';
+
+	export default {
+		name: 'NeutronMain',
+		components: {
+			RequestHistory,
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	@import '../assets/utility';
+
+	#main {
+		@include no-padding;
+	}
+
+	#main_row {
+		@include max-height;
+	}
+
+	.bordered-column {
+		border-style: solid;
+		border-width: 0 1px 0 0;
+	}
+
+</style>
