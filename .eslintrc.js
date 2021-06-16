6645451ca1aaf474df0ee98f25e8534fda38e1e3
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2021-06-16 22:55:39 +0100

    added rule to allow crlf

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-tabs': ['error', { allowIndentationTabs: true }],
+    'linebreak-style': ['error', 'windows'],
     'vue/script-indent': ['error', 'tab', { baseIndent: 1 }],
     'vue/html-indent': ['error', 'tab', { baseIndent: 1 }],
   },
